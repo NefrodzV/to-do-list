d29f718d87203466db1619104a7a75b7a602fe6b
Neftaly <neftalyrv23@gmail.com>
2023-07-02 18:46:11 -0400

    Updated code to have the style file running with the build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = "Running Webpack with watcher";
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+import  './style.css';
+
+
   
