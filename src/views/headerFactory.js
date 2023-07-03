610b79e766b8d2b2b8d9b1e6e1b195c41b883ed2
Neftaly <neftalyrv23@gmail.com>
2023-07-03 10:15:34 -0400

    Added the h1 for the header section

--- src/views/headerFactory.js
+++ src/views/headerFactory.js
@@ -4,9 +4,10 @@ import ElementWriter from "../ElementWriter";
 export default function headerFactory() {
 
     const headerElement = document.createElement('header');
+    const title = document.createElement('h1');
+    ElementWriter.write(title, 'Todo List');
     
-    ElementWriter.write(headerElement, 'Todo List')
-
+    headerElement.appendChild(title);
     return {
         headerElement
     }
