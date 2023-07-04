4e9b125f21169614374c07addeafbfaab9e0f3ce
Neftaly <neftalyrv23@gmail.com>
2023-07-04 15:34:45 -0400

    Added logic to change layer visibility

--- src/ui/layerFactory.js
+++ src/ui/layerFactory.js
@@ -1,9 +1,17 @@
+import events from "../events";
+
 export default function layerFactory() {
 
     const LAYER_STYLE = 'layer';
 
     const layerElement = document.createElement('div');
-    layerElement.classList.add('layer');
+    layerElement.classList.add(LAYER_STYLE);
+
+    const toggleVisiibility = () => {
+        layerElement.toggleAttribute('visible');
+    }
+
+    events.on('updateLayerVisibility', toggleVisiibility);
 
     return layerElement; 
 }
