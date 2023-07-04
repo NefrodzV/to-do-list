e077951a8f426a407ea563888d6d3741dcfdba22
Neftaly <neftalyrv23@gmail.com>
2023-07-04 15:32:31 -0400

    Class to comunucated between modules

---
+++ src/events.js
@@ -0,0 +1,31 @@
+
+let events = {
+    
+    _events: [],
+
+    on: function(eventName, fn) {
+        this._events[eventName] = this._events[eventName] || [];
+        this._events[eventName].push(fn)
+        console.log(this._events);
+    },
+
+    off: function(eventName, fn) {
+        if(this._events[eventName]) {
+            for(let i = 0;i < this._events[eventName].lenght; i ++) {
+                if(this._events[eventName][i] === fn) {
+                    this._events[eventName].splice(i, 1);
+                }
+            }
+        }
+    },
+
+    emit: function(eventName, data) {
+        if(this._events[eventName]) {
+            this._events[eventName].forEach(function(fn){
+                fn(data);
+            });
+        }
+    }
+}
+
+export default events;
