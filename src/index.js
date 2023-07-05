963ea58f223ef3f044995816b2233981b18d2edb
Neftaly <neftalyrv23@gmail.com>
2023-07-05 17:05:32 -0400

    Added layer element

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import event from './events';
 import  './style.css';
 import footerFactory from './ui/footerFactory';
 import headerFactory from './ui/headerFactory';
+import layerFactory from './ui/layer/layerFactory';
 import mainFactory from './ui/mainFactory';
 import sidebarFactory from './ui/sidebar/sidebarFactory';
 
@@ -9,11 +10,13 @@ const header = headerFactory();
 const sideBar = sidebarFactory();
 const main = mainFactory();
 const footer = footerFactory();
+const layer = layerFactory();
 
 document.body.appendChild(header);
 document.body.appendChild(sideBar);
 document.body.appendChild(main);
 document.body.appendChild(footer);
+document.body.appendChild(layer);
 
 
 
