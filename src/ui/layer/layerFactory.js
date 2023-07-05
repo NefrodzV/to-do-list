327eb559a9eab7bb3d76f117aeebc4b7974cc180
Neftaly <neftalyrv23@gmail.com>
2023-07-05 14:02:04 -0400

    Created container

--- src/ui/layer/layerFactory.js
+++ src/ui/layer/layerFactory.js
@@ -3,12 +3,28 @@ import events from "../../events";
 export default function layerFactory() {
 
     const LAYER_STYLE = 'layer';
+    const CONTAINER_STYLE = 'layer-container';
 
     const layerElement = document.createElement('div');
     layerElement.classList.add(LAYER_STYLE);
 
+    const closeButton = document.createElement('button');
+    closeButton.textContent = "X";
+
+    const container = document.createElement('div');
+    container.classList.add(CONTAINER_STYLE);
+
+    container.appendChild(closeButton);
+    layerElement.appendChild(container);
+    
+    
     const toggleVisibility = () => {
         layerElement.toggleAttribute('visible');
+    }
+
+    toggleVisibility();
+    const updateContainerElements = () => {
+        
     }
 
     events.on('updateLayerVisibility', toggleVisibility);
