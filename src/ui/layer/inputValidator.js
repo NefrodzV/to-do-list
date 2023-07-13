ceef27fea388e68e0de8133a36e56400ea52d590
Neftaly <neftalyrv23@gmail.com>
2023-07-13 19:45:26 -0400

    remove logs

--- src/ui/layer/inputValidator.js
+++ src/ui/layer/inputValidator.js
@@ -4,7 +4,6 @@ export default function inputValidator(inputs) {
     for(let i = 0; i < inputs.length; i++) {
         let input = inputs[i];
         let data = input.value;
-        console.log(data);
         if(data === '') {
             events.emit('displayInputError', input);
         }
