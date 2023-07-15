99a5cf3bb9a81932082834acc22309f056511462
Neftaly <neftalyrv23@gmail.com>
2023-07-15 15:05:44 -0400

    model for todo

--- src/model/createTodo.js
+++ src/model/createTodo.js
@@ -1,3 +1,26 @@
-export function createTodo(title, description, date) {
+export default function createTodo(title, description, date, completeState = false) {
+
+
+
+    const getTitle  = ()  =>{
+        return title;
+    }
+
+    const getDescription = () => {
+        return description;
+    }
+
+    const getDate = () => {
+        return date;
+    } 
+
+    const getCompleteState = () => {
+        return completeState;
+    }
     
+    return {
+        getTitle, 
+        getDescription,
+        getDate
+    }
 }
