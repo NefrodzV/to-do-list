db4a31f88ad25ed3b0330b7a88f9a3ad4f11e015
Neftaly <neftalyrv23@gmail.com>
2023-07-16 19:30:05 -0400

    list for project items

---
+++ src/ui/main/projectListComponent.js
@@ -0,0 +1,44 @@
+export default function projectListComponent(projects, itemCallback) {
+    console.log(projects);
+
+    const ul = document.createElement('ul');
+
+    (() => {
+        if(projects.length === 0 || projects === null) return;
+        for(let i = 0; i < projects.length; i++) {
+            let item = projects[i];
+            updateList(item);
+        }
+    })();
+
+    function updateList(project) {
+        const item = projectItemComponent(project.id, project.title, itemCallback);
+        ul.appendChild(item);
+    }
+
+    return {
+        getList() {
+            return ul;
+        }
+    }
+}
+
+function projectItemComponent(id, title, itemCallback) {
+    const STYLE = 'project';
+
+    const listItem = document.createElement('li');
+    listItem.classList.add(STYLE);
+    listItem.addEventListener('click', () => {
+        itemCallback(id);
+    })
+
+    const titlePara = document.createElement('p');
+    titlePara.textContent = title;
+
+    const completionSpan = document.createElement('span');
+    completionSpan.textContent = '8/12';
+
+    listItem.append(titlePara, completionSpan);
+
+    return listItem;
+}
