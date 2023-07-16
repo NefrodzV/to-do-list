725c31b50b8eab181289429be33b20a7f8b5d414
Neftaly <neftalyrv23@gmail.com>
2023-07-16 19:30:59 -0400

    Adding the list component

--- src/ui/main/mainFactory.js
+++ src/ui/main/mainFactory.js
@@ -1,11 +1,17 @@
+import projectRepository from "../../data/repository/projectRepository";
+import projectListComponent from "./projectListComponent";
 
 
 export default function mainFactory() {
 
     const mainElement = document.createElement('main');
+    const listComponent = projectListComponent(projectRepository.getAllProjects(), (id) => {
+        console.log('item id clicked is: ' + id)
+        console.log('view project data flow');
+    })
+    const projectList = listComponent.getList();
     
-    
-
+    mainElement.appendChild(projectList);
     
     return mainElement;
 }
