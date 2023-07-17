1b1f0e5e1407855aa3c820d19aa4c84bc2225009
Neftaly <neftalyrv23@gmail.com>
2023-07-17 19:24:23 -0400

    updated code

--- src/ui/main/mainFactory.js
+++ src/ui/main/mainFactory.js
@@ -5,13 +5,19 @@ import projectListComponent from "../listComponents/projectListComponent";
 export default function mainFactory() {
 
     const mainElement = document.createElement('main');
+    const contentHeader = document.createElement('h2');
+    contentHeader.classList.add('list-header');
+    contentHeader.textContent = 'Inbox';
+
     const listComponent = projectListComponent(projectRepository.getAllProjects(), (id) => {
         console.log('item id clicked is: ' + id)
         console.log('view project data flow');
-    })
+    });
+
     const projectList = listComponent.getList();
     
+    mainElement.appendChild(contentHeader);
     mainElement.appendChild(projectList);
-    
+   
     return mainElement;
 }
