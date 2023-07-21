de816c1a0c911414959020b71e3e813206fa456d
Neftaly <neftalyrv23@gmail.com>
2023-07-21 19:23:59 -0400

    delete code unused

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import  './style.css';
 import footerFactory from './ui/footerFactory';
 import headerFactory from './ui/headerFactory';
-import layerFactory from './ui/layer/layerFactory';
 import mainFactory from './ui/main/mainFactory';
 import sidebarFactory from './ui/sidebar/sidebarFactory';
 
 const header = headerFactory();
-const sideBar = sidebarFactory();
 const main = mainFactory();
+const sideBar = sidebarFactory();
+
 const footer = footerFactory();
-// const layer = layerFactory();
+
 
 document.body.appendChild(header);
 document.body.appendChild(sideBar);
 document.body.appendChild(main);
 document.body.appendChild(footer);
-// document.body.appendChild(layer);
+
 
 
