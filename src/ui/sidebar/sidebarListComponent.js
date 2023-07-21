9b681b8b40a3f960de19baac9a43718f48f6e737
Neftaly <neftalyrv23@gmail.com>
2023-07-21 19:23:44 -0400

    component for sidebar items

---
+++ src/ui/sidebar/sidebarListComponent.js
@@ -0,0 +1,40 @@
+import sidebarItemsFactory from "./sidebarItemsFactory";
+
+
+import events from '../../events';
+
+export default function sidebarListComponent() {
+
+    const sidebarItems = sidebarItemsFactory();
+
+    const ul = document.createElement('ul');
+
+    (() => {
+        if(!Array.isArray(sidebarItems) || sidebarItems.length === 0 || sidebarItems === null) return;
+        updateList();
+    })();
+
+    function addItem(item) {
+        ul.appendChild(item);
+    }
+
+    function updateList() {
+        for(let i = 0; i < sidebarItems.length; i++) {
+            
+            let sidebarItem = sidebarItems[i];
+            addItem(sidebarItem.getElement());
+            
+        }
+    }
+
+    function disableActiveItems() {
+        sidebarItems.forEach(item => {
+            item.disableActive();
+        });
+    }
+
+    events.on('sidebarItemClicked', disableActiveItems);
+
+    return ul;
+
+}
