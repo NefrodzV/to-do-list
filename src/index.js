3f13e7012584578f484da8aa17480b25a0c69aeb
Neftaly <neftalyrv23@gmail.com>
2023-07-23 18:31:31 -0400

    update code

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import  './style.css';
 import footerFactory from './ui/footerFactory';
 import headerFactory from './ui/headerFactory';
+import layerFactory from './ui/layer/layerFactory';
 import mainFactory from './ui/main/mainFactory';
 import sidebarFactory from './ui/sidebar/sidebarFactory';
 
 const header = headerFactory();
-const main = mainFactory();
 const sideBar = sidebarFactory();
-
+const main = mainFactory();
 const footer = footerFactory();
-
 
 document.body.appendChild(header);
 document.body.appendChild(sideBar);
 document.body.appendChild(main);
 document.body.appendChild(footer);
+
 
 
 
