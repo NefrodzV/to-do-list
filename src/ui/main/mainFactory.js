85533186403877824c8654a9f6a6cf396543571f
Neftaly <neftalyrv23@gmail.com>
2023-07-23 13:37:02 -0400

    initialized the main content

--- src/ui/main/mainFactory.js
+++ src/ui/main/mainFactory.js
@@ -1,22 +1,27 @@
 import events from "../../events";
+import inboxFactory from "./inboxFactory";
 
 export default function mainFactory() {
 
     const mainElement = document.createElement('main');
+    //Will only run once for when loading page
+    (() => {
+        mainElement.appendChild(inboxFactory());
+    })();
     
     function update(element) {
         removeElements();
-
         mainElement.appendChild(element);
     }
 
     // Used to communicate when menu items are clicked
     events.on('updateMain', update);
-
+   
     function removeElements() {
         while(mainElement.firstChild) {
             mainElement.removeChild(mainElement.firstChild);
         }
     }
+    
     return mainElement;
 }
