933d2ceb493b60117245f3b05f2fc097c8f413af
Neftaly <neftalyrv23@gmail.com>
2023-07-23 17:34:14 -0400

    layer for window for single todo item view

--- src/ui/main/mainFactory.js
+++ src/ui/main/mainFactory.js
@@ -1,12 +1,16 @@
 import events from "../../events";
+import layerFactory from "../layer/layerFactory";
 import inboxFactory from "./inboxFactory";
 
 export default function mainFactory() {
 
     const mainElement = document.createElement('main');
+    const layer = layerFactory();
+    
     //Will only run once for when loading page
     (() => {
         mainElement.appendChild(inboxFactory());
+        mainElement.appendChild(layer);
     })();
     
     function update(element) {
