9ebb79ed7fbd42972f8ebb7a5ac2368b09bf5658
Neftaly <neftalyrv23@gmail.com>
2023-07-23 18:32:01 -0400

    update code for layer element

--- src/ui/layer/layerFactory.js
+++ src/ui/layer/layerFactory.js
@@ -1,29 +1,26 @@
 import events from "../../events";
-import addProjectFormFactory from "./addProjectFormFactory";
+import projectDetails from "./projectDetails";
+
 
 
 export default function layerFactory() {
+
+
 
     const LAYER_STYLE = 'layer';
     const CONTAINER_STYLE = 'layer-container';
 
     const layerElement = document.createElement('div');
     layerElement.classList.add(LAYER_STYLE);
-
-    const toggleVisibility = () => {layerElement.toggleAttribute('visible');}
     
-    const closeButton = document.createElement('button');
-    closeButton.textContent = "X";
-    closeButton.addEventListener('click',toggleVisibility);
-    
-    const container = document.createElement('div');
-    container.classList.add(CONTAINER_STYLE);
-    container.classList.add('corner');
 
-    let form = addProjectFormFactory();
-    container.appendChild(closeButton);
-    container.appendChild(form);
-    layerElement.appendChild(container);
+    const toggleVisibility = () => { layerElement.toggleAttribute('visible'); }
+    
+    const projectDetail = projectDetails(() => {
+        toggleVisibility();
+    }); 
+
+    layerElement.appendChild(projectDetail);
 
     events.on('updateLayerVisibility', toggleVisibility);
 
