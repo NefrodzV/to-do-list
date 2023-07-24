54309ca041c28f70b91e2d09ee0cdae24b705c43
Neftaly <neftalyrv23@gmail.com>
2023-07-23 22:37:22 -0400

    set logic to update the window

--- src/ui/layer/projectDetails.js
+++ src/ui/layer/projectDetails.js
@@ -1,13 +1,16 @@
+import projectRepository from "../../data/repository/projectRepository";
 import events from "../../events";
 import todoListComponent from "../listComponents/todoListComponent";
 
-
 export default function projectDetails(closeFunction) {
+
+    let project;
     
     const CONTAINER_STYLE = 'layer-container';
 
     const container = document.createElement('div');
     container.classList.add(CONTAINER_STYLE);
+
     const closeButton = document.createElement('button');
     closeButton.classList.add('close-button');
     closeButton.textContent = "X";
@@ -22,7 +25,22 @@ export default function projectDetails(closeFunction) {
 
     const todosListComponent = todoListComponent();
 
-    container.append(closeButton, title, description, todosListComponent);
+    function update() {
+        title.textContent = project.getTitle();
+        description.textContent = project.getDescription();
+        todosListComponent.updateList(project.getTodos());
+    }
+    function getDataFromSource(id) {
+        project = projectRepository.getProjectWithId(id);
+        console.log(project);
+        update();
+    }
+
+    
+
+    events.on('updateProjectDetails', getDataFromSource);
+
+    container.append(closeButton, title, description, todosListComponent.getListElement());
 
     return container;
 }
