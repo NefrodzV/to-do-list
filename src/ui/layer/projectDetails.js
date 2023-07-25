2807b5263c93060f1e161edbbf0fa165a24599ec
Neftaly <neftalyrv23@gmail.com>
2023-07-25 16:18:28 -0400

    Add button todo  and edit fields functionality

--- src/ui/layer/projectDetails.js
+++ src/ui/layer/projectDetails.js
@@ -1,8 +1,11 @@
 import projectRepository from "../../data/repository/projectRepository";
 import events from "../../events";
+import Transform from "../../utils/Transform";
 import todoListComponent from "../listComponents/todoListComponent";
 
 export default function projectDetails(closeFunction) {
+
+    const transform = Transform();
 
     let project;
     
@@ -16,18 +19,43 @@ export default function projectDetails(closeFunction) {
     closeButton.textContent = "X";
     closeButton.addEventListener('click', () => {
         closeFunction();
+        
     });
 
     const title = document.createElement('p');
     title.classList.add('layer-title');
-    title.textContent = "Project title";
+    title.addEventListener('click', () => {
+        transform.replaceElement(title, 'input','text', project.getTitle(), (value) => {
+            project.setTitle(value);
+            update(title, project.getTitle());
+            updateProject();
+            events.emit('updateProjectList', projectRepository.getAllProjects());
+        })
+    })
     const description = document.createElement('p');
     description.classList.add('layer-description');
-    description.textContent = 'project description';
+    description.addEventListener('click', () => {
+        transform.replaceElement(description, 'input','text', project.getDescription(), (value) => {
+            project.setDescription(value);
+            update(description, project.getDescription());
+            updateProject();
+            
+        })
+    })
 
     const todosListComponent = todoListComponent();
 
-    function update() {
+    const addTodoButton = document.createElement('button');
+    addTodoButton.classList.add('add-todo-button');
+    addTodoButton.textContent = '+ Add Todo';
+    addTodoButton.addEventListener('click', () => {
+        todosListComponent.addInputComponent();
+    });
+
+    function update(target, value) {
+        target.textContent = value;
+    }
+    function updateFields() {
         title.textContent = project.getTitle();
         description.textContent = project.getDescription();
         todosListComponent.updateList(project.getTodos());
@@ -35,12 +63,23 @@ export default function projectDetails(closeFunction) {
     function getDataFromSource(id) {
         project = projectRepository.getProjectWithId(id);
         console.log(project);
-        update();
+        updateFields();
     }
     
+    function addTodoToProject(todo) {
+        project.addTodo(todo);
+        updateProject();
+    }
+    events.on('addTodo', addTodoToProject);
     events.on('updateProjectDetails', getDataFromSource);
 
-    container.append(closeButton, title, description, todosListComponent.getListElement());
+    function updateProject() {
+        projectRepository.updateProject(project);
+    }
+    
+    events.on('updateProject', updateProject);
+
+    container.append(closeButton, title, description, todosListComponent.getListElement(), addTodoButton);
 
     return container;
 }
