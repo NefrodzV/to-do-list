0543423645bca86845c7f0113bf5475cd79cd485
Neftaly <neftalyrv23@gmail.com>
2023-07-27 21:33:01 -0400

    Fixed sort logic to sort if default date string is displayed

--- src/utils/Sorter.js
+++ src/utils/Sorter.js
@@ -3,7 +3,8 @@ import { compareAsc, parseISO } from "date-fns";
 export default function Sorter() {
     const sortByDate = (items) => {
         items.sort((firstItem, secondItem) => {
-            if (firstItem.getDate() === 'No date set') {
+            // Will compare the dates and see if its the init message for date if so move it last
+            if (firstItem.getDate() === 'No date set' && secondItem !== 'No date set') {
                 console.log('No date set');
                 return 1;
             }
