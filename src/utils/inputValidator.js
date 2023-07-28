6b3ff75aa6f59bc52348a7a123062c779a367094
Neftaly <neftalyrv23@gmail.com>
2023-07-27 20:58:59 -0400

    moved the input validator

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -1,5 +1,6 @@
-import events from "../../events";
+import events from "../events";
 
+// Will validate and array of inputs
 export default function inputValidator(inputs) {
     for(let i = 0; i < inputs.length; i++) {
         let input = inputs[i];
