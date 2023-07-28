31e4f110baf5a521de06f3c59e37b6f3b217a6d5
Neftaly <neftalyrv23@gmail.com>
2023-07-27 21:05:35 -0400

    Refactor and deleted to menu items

--- src/ui/sidebar/sidebarItemsFactory.js
+++ src/ui/sidebar/sidebarItemsFactory.js
@@ -1,6 +1,4 @@
 import inboxPNG from '../../asset/icon-inbox.png';
-import upcomingPNG from '../../asset/icon-upcoming.png';
-import todayPNG from '../../asset/icon-today.png';
 import addPGN from '../../asset/icon-add.png';
 import inboxFactory from '../main/inboxFactory';
 import sidebarItemFactory from './sidebarItemFactory';
@@ -10,11 +8,9 @@ export default function sidebarItemsFactory() {
 
     const sidebarItems = [
         sidebarItemFactory(inboxPNG, 'inbox', inboxFactory(), true),
-        sidebarItemFactory(upcomingPNG, 'upcoming'),
-        sidebarItemFactory(todayPNG, 'today'),
         sidebarItemFactory(addPGN, 'new project', projectFormFactory()),
     ];
-
+    
     return sidebarItems;
 }
 
